1626ef05f277f5a57401ee2b2ce05a99b7373b18
Siddhanth B N <bnsiddhanth0@gmail.com>
2023-01-30 23:08:26 +0530

    fix: udp key server

--- unusual_database_program/src/main.rs
+++ unusual_database_program/src/main.rs
@@ -1,9 +1,8 @@
-use std::{net::{UdpSocket, SocketAddr}, collections::HashMap};
-use protohackers_tcp_helper::{
-    cli_helper::Args
-};
+use std::{net::UdpSocket, collections::HashMap};
+use protohackers_tcp_helper::cli_helper::Args;
 use clap::Parser;
 
+#[derive(Debug)]
 struct UnusualDatabase {
     key_store: HashMap<String, String>
 }
@@ -27,13 +26,13 @@ fn server_init(port: u16) -> Result<UdpSocket, std::io::Error> {
     Ok(UdpSocket::bind(format!("0.0.0.0:{port}"))?)
 }
 
-fn find_equals_index(buf: &[u8]) -> Option<usize> {
-    buf.iter().position(|&c| c == b'=')
+fn find_char_index(buf: &[u8], byte: u8) -> Option<usize> {
+    buf.iter().position(|&c| c == byte)
 }
 
 fn receive_datagram(socket: &UdpSocket, buf: &mut [u8]) -> Result<(usize, String), std::io::Error> {
     let (num_bytes, src_addr) = socket.recv_from(buf)?;
-    println!("Source:{};Num Bytes:{}", src_addr, num_bytes);
+    println!("Source:{};Num Bytes:{};Buf::{:?}", src_addr, num_bytes, &buf[..(num_bytes - 1)]);
     Ok((num_bytes, src_addr.to_string()))
 }
 
@@ -47,7 +46,7 @@ fn main() {
     let udp_socket = server_init(args.port).expect("Failed to bind to port");
     let mut unusual_database = UnusualDatabase::new();
     loop {
-        let mut buf = vec![];
+        let mut buf = [0; 1000];
         let (bytes_read, src_addr) = match receive_datagram(&udp_socket, &mut buf) {
             Ok(data) => data,
             Err(_) => (0, String::from("")),
@@ -55,18 +54,20 @@ fn main() {
         if bytes_read == 0 {
             continue;
         }
-        println!("Data: {:?}", buf);
         let buf_length = buf.len();
-        let equals_index = find_equals_index(&buf).unwrap_or_else(|| buf_length);
+        let equals_index = find_char_index(&buf, b'=').unwrap_or_else(|| buf_length);
         if equals_index == buf_length {
-            let mut key = String::from_utf8(buf).unwrap();
+            let mut key = String::from_utf8(buf[..bytes_read - 1].into()).unwrap();
             let response = match unusual_database.retrieve_key(&key) {
                 Some(value) => {
-                    key.push_str(value);
+                    key.push('=');
+                    key.push_str(&format!("{value}\n"));
+                    // key.replace("\n", "")
                     key
                 }
                 None => {
-                    key.push('=');
+                    key.push_str(&format!("=\n"));
+                    // key.replace("\n", "")
                     key
                 }
             };
@@ -80,8 +81,8 @@ fn main() {
             key = String::from("");
             value = String::from("");
         } else {
-            key = String::from_utf8(buf[..equals_index].to_vec()).unwrap_or_else(|_| String::from(""));
-            value = String::from_utf8(buf[equals_index + 1..].to_vec()).unwrap_or_else(|_| String::from(""));
+            key = String::from_utf8(buf[..equals_index].to_vec()).unwrap_or_else(|_| String::from("")).replace("\n", ""); 
+            value = String::from_utf8(buf[equals_index + 1..(bytes_read - equals_index)].to_vec()).unwrap_or_else(|_| String::from("\n"));
         }
         unusual_database.insert_key(key, value);
     }
