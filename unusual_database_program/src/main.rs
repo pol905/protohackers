71c920f4b33400d6e99c2958b4594e87cd9bd84c
Siddhanth B N <bnsiddhanth0@gmail.com>
2023-02-05 01:22:10 +0530

    fix: revert bind addr

--- unusual_database_program/src/main.rs
+++ unusual_database_program/src/main.rs
@@ -34,7 +34,7 @@ fn find_char_index(buf: &[u8], byte: u8) -> Option<usize> {
 
 fn receive_datagram(socket: &UdpSocket, buf: &mut [u8]) -> Result<(usize, String), std::io::Error> {
     let (num_bytes, src_addr) = socket.recv_from(buf)?;
-    // println!("Source:{};Num Bytes:{};Buf::{:?}", src_addr, num_bytes, &buf[..(num_bytes)]);
+    println!("Source:{};Num Bytes:{};Buf::{:?}", src_addr, num_bytes, &buf[..(num_bytes)]);
     Ok((num_bytes, src_addr.to_string()))
 }
 
